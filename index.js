332bda73d857f99f857084acb9db8c3594be7fb3
prez <patodeath@gmail.com>
2021-12-13 02:01:20 -0300

    refactor(index.js): refactor index.js code

--- index.js
+++ index.js
@@ -2,73 +2,75 @@ let monthDiv = document.querySelector("#month");
 let yearDiv = document.querySelector("#year");
 let weekDaysTr = document.querySelector("#weekDays");
 let tbody = document.querySelector("tbody");
+let daysLables = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+let monthsLabels = ['Enero', 'Febrero', 'Marzo',
+    'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+    'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
 function generateCalendar(month, year) {
-    var now = new Date();
-
-    var days_labels = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
-        months_labels = ['Enero', 'Febrero', 'Marzo',
-            'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
-            'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    let now = new Date();
 
     this.month = (isNaN(month) || month == null) ? now.getMonth() + 1 : month;
     this.year = (isNaN(year) || year == null) ? now.getFullYear() : year;
 
-    var logical_month = this.month - 1;
+    let logicalMonth = this.month - 1;
 
-    var first_day = new Date(this.year, logical_month, 1),
-        first_day_weekday = first_day.getDay() == 0 ? 7 : first_day.getDay();
+    let firstDay = new Date(this.year, logicalMonth, 1);
+    let firstDayWeekday = firstDay.getDay() == 0 ? 7 : firstDay.getDay();
 
-    var month_length = new Date(this.year, this.month, 0).getDate(),
-        previous_month_length = new Date(this.year, logical_month, 0).getDate();
+    let monthLength = new Date(this.year, this.month, 0).getDate();
+    let previousMonthLength = new Date(this.year, logicalMonth, 0).getDate();
 
-    monthDiv.innerHTML = months_labels[logical_month];
+    monthDiv.innerHTML = monthsLabels[logicalMonth];
     yearDiv.innerHTML = this.year;
 
-    // Generating day titles
-    for (var i = 0; i <= 6; i++) {
-        let dayTh = document.createElement("th");
-        dayTh.innerHTML = days_labels[i];
-        weekDaysTr.appendChild(dayTh);
-    }
+    generateDaysTitles();
 
-    var day = 1, 
-        prev = 1, 
-        next = 1;
+    let day = 1
+    let prev = 1
+    let next = 1;
 
-    
-    for (var i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         let weekTr = document.createElement("tr");
         weekTr.className = "week";
 
-        for (var j = 1; j <= 7; j++) {
-            if (day <= month_length && (i > 0 || j >= first_day_weekday)) {
-                let dayTd = document.createElement("td");
-                dayTd.className = "day";
-                dayTd.innerHTML = day;
-                weekTr.appendChild(dayTd);
+        for (let j = 1; j <= 7; j++) {
+            if (day <= monthLength && (i > 0 || j >= firstDayWeekday)) {
+                weekTr.appendChild(generateDayTd(day, "day"));
                 day++;
-            } else if (day <= month_length) {
-                let dayTd = document.createElement("td");
-                dayTd.className = "day otherMonth";
-                dayTd.innerHTML = previous_month_length - first_day_weekday + prev + 1;
-                weekTr.appendChild(dayTd);
+            } else if (day <= monthLength) {
+                let html = previousMonthLength - firstDayWeekday + prev + 1;
+                weekTr.appendChild(generateDayTd(html, "day otherMonth"));
                 prev++;
             } else {
-                let dayTd = document.createElement("td");
-                dayTd.className = "day otherMonth";
-                dayTd.innerHTML = next;
-                weekTr.appendChild(dayTd);
+                weekTr.appendChild(generateDayTd(next, "day otherMonth"));
                 next++;
             }
         }
 
         tbody.appendChild(weekTr);
 
-        if (day > month_length) {
+        if (day > monthLength) {
             break;
-        } 
+        }
     }
 }
 
-generateCalendar(11, 2021);
+function generateDaysTitles() {
+    for (let i = 0; i <= 6; i++) {
+        let dayTh = document.createElement("th");
+        dayTh.innerHTML = daysLables[i];
+        weekDaysTr.appendChild(dayTh);
+    }
+}
+
+function generateDayTd(html, className) {
+    let dayTd = document.createElement("td");
+
+    dayTd.className = className;
+    dayTd.innerHTML = html;
+
+    return dayTd;
+}
+
+generateCalendar();
